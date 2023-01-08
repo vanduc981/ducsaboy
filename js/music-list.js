c027ca2251a9ac8346f272dcb4de1ca59b999f8c
VAN DUC <dvan98679@gmail.com>
2023-01-08 13:27:22 +0700

    Release suntech frontend final project

---
+++ js/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [{
+        name: "CON HỨA SẼ VỀ",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "ƯỚC MƠ CỦA MẸ",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "ĐƠN GIẢN YÊU EM ",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "THẬT RA ANH ĐÂU...",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "2",
+        src: "2"
+    },
+    {
+        name: "CỐ GẮNG LÊN EM ƠI",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "3",
+        src: "3"
+    },
+    {
+        name: "MÌNH NGHÈO",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "4",
+        src: "4"
+    },
+    {
+        name: "1234 NGÀY",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "5",
+        src: "5"
+    },
+    {
+        name: "LỜI CHƯA NÓI",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "6",
+        src: "6"
+    },
+    {
+        name: "Sợ Ngày Mai Em Đi Mất",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "7",
+        src: "7"
+    },
+    {
+        name: "NHƯ ANH ĐÃ THẤY EM",
+        artist: "NGUYỄN VĂN ĐỨC",
+        img: "8",
+        src: "8"
+    },
+    {
+        name: "MÌNH CÙNG NHAU ĐÓNG BĂNG",
+        artist: "NGUYỄN VĂN ĐỨC 10A4",
+        img: "9",
+        src: "9"
+    },
+];
